f6b7a8381bcd5a1e5401602999ff689b74a97e65
Niklas Ingholt <niklas@ingholt.com>
2011-06-29 23:12:23 +0200

    Initial commit

---
+++ PartilleTennis/PartilleTennisAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PartilleTennisAppDelegate.h
+//  PartilleTennis
+//
+//  Created by Niklas Ingholt on 2011-06-29.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PartilleTennisAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PartilleTennis/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  PartilleTennis
+//
+//  Created by Niklas Ingholt on 2011-06-29.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PartilleTennisAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	int retVal = 0;
+	@autoreleasepool {
+	    retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([PartilleTennisAppDelegate class]));
+	}
+	return retVal;
+}
