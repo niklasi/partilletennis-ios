bdf5d38ab3ab00545139c2cf12b6ba3e906a0fae
Niklas Ingholt <niklas@ingholt.com>
2011-08-09 23:46:57 +0200

    Revert "Removed some file because of structure change."
    This reverts commit a87fb7478c42d1982ae7977a60c7ec1322644cfa.

---
+++ PartilleTennis/MatchesController.m
@@ -0,0 +1,195 @@
+//
+//  MatchesController.m
+//  PartilleTennis
+//
+//  Created by Niklas Ingholt on 2011-07-01.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "MatchesController.h"
+#import "Match.h"
+#import "DSActivityView.h"
+#import "MatchDetailController.h"
+
+@interface MatchesController() {
+}
+
+@property (nonatomic, strong) Team *currentTeam;
+@end
+
+@implementation MatchesController
+
+@synthesize matchData = _matchData, matchTableCell = _matchTableCell, currentTeam = _currentTeam;
+
+-(id)init
+{
+    self = [super initWithStyle:UITableViewStylePlain];
+    if (self) {
+			self.title = @"Matcher";
+			pfService = [[PfService alloc] init];
+			pfService.delegate = self;
+			
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+	[super viewDidLoad];
+	NSLog(@"View load...");
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+	[super viewWillAppear:animated];
+	
+	id<TeamDelegateProtocol> teamDelegate = (id<TeamDelegateProtocol>) [UIApplication sharedApplication].delegate;
+	Team *myTeam = teamDelegate.myTeam;
+
+	if (self.matchData.count == 0 || ![myTeam isEqual: self.currentTeam]) {
+		self.currentTeam = myTeam;
+		[DSActivityView newActivityViewForView:self.view withLabel:@"Laddar..."].showNetworkActivityIndicator = YES;
+		NSLog(@"Division: %d, Team: %d", myTeam.division, myTeam.ranking);
+		[pfService loadMatches:myTeam.division team:myTeam.ranking];
+	}
+}
+
+- (void)loadedMatches:(NSArray *)matches
+{
+	[DSActivityView removeView];
+	self.matchData = matches;
+	[self.tableView reloadData];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return self.matchData.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	NSString *cellIdentifier = @"UpcomingMatchCell";
+	NSString *nib = @"UpcomingMatchTableCellView";	
+	
+	if (indexPath.row < 3) {
+		cellIdentifier = @"CompletedMatchCell";
+		nib = @"CompletedMatchTableCellView";
+	}
+	
+	MatchTableCell *cell = (MatchTableCell *)[tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+	if (cell == nil) {
+		//cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+		[[NSBundle mainBundle] loadNibNamed:nib owner:self options:nil];
+		cell = self.matchTableCell;
+	}
+    
+	Match *match = (Match *)[[self matchData] objectAtIndex:[indexPath row]];
+	cell.match = match;
+	return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	// Navigation logic may go here. Create and push another view controller.
+	MatchDetailController *matchDetailController = [[MatchDetailController alloc] init];
+	Match *match = (Match *)[self.matchData objectAtIndex:indexPath.row];
+	[matchDetailController setContact:match.contact];
+	
+	[self.navigationController pushViewController:matchDetailController animated:YES];
+	
+}
+
+@end
---
+++ PartilleTennis/PartilleTennisAppDelegate.m
@@ -0,0 +1,100 @@
+//
+//  PartilleTennisAppDelegate.m
+//  PartilleTennis
+//
+//  Created by Niklas Ingholt on 2011-06-29.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "PartilleTennisAppDelegate.h"
+#import "SeriesController.h"
+#import "MatchesController.h"
+#import "SettingsController.h"
+#import "TeamPickerController.h"
+
+@implementation PartilleTennisAppDelegate
+
+@synthesize window = _window, navController, matchesController, settingsController, myTeam, allTeams;
+
+-(id)init
+{
+	self.myTeam = [NSKeyedUnarchiver unarchiveObjectWithFile:pathInDocumentDirectory(@"myTeam")];
+	return [super init];
+}
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    // Override point for customization after application launch.
+	self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+	
+	UITabBarController *tabBarController = [[UITabBarController alloc] init];
+	UIViewController *seriesController = [[SeriesController alloc] init];
+	self.navController = [[UINavigationController alloc] initWithRootViewController:seriesController];
+	self.navController.navigationBar.barStyle = UIBarStyleBlackOpaque;
+
+	self.matchesController = [[UINavigationController	alloc] initWithRootViewController:
+														[[MatchesController alloc] init]];
+	self.matchesController.navigationBar.barStyle = UIBarStyleBlackOpaque;
+	
+	self.settingsController = [[UINavigationController alloc] initWithRootViewController:
+																														[[SettingsController alloc] init]];
+	self.settingsController.navigationBar.barStyle = UIBarStyleBlackOpaque;
+	NSArray *viewControllers = [NSArray arrayWithObjects:self.navController, self.matchesController, self.settingsController, nil];
+	
+	[tabBarController setViewControllers:viewControllers];
+	UITabBarItem *seriesTableItem = [tabBarController.tabBar.items objectAtIndex:0];
+	seriesTableItem.image = [UIImage imageNamed:@"104-index-cards.png"]; 
+	UITabBarItem *matchesItem = [tabBarController.tabBar.items objectAtIndex:1];
+	matchesItem.image = [UIImage imageNamed:@"83-calendar.png"]; 
+	UITabBarItem *settingsItem = [tabBarController.tabBar.items objectAtIndex:2];
+	settingsItem.image = [UIImage imageNamed:@"19-gear.png"]; 
+  [_window setRootViewController:tabBarController];
+	
+	if (self.myTeam == nil) {
+		UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Välj lag" message:@"Du måste välja vilket lag du tillhör." delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+		[alert show];
+		tabBarController.selectedIndex = 2;
+		TeamPickerController *picker = [[TeamPickerController alloc] initWithNibName:@"TeamPickerView" bundle:nil];
+		[self.settingsController pushViewController:picker animated:YES];
+	}
+	
+	// Add the navigation controller's view to the window and display.
+	[_window addSubview:tabBarController.view];
+
+	[self.window makeKeyAndVisible];
+	return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+	/*
+	 Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+	 Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+	 */
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+	[NSKeyedArchiver archiveRootObject:self.myTeam toFile:pathInDocumentDirectory(@"myTeam")];
+	self.myTeam = nil;
+	self.allTeams = nil;
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+	self.myTeam = [NSKeyedUnarchiver unarchiveObjectWithFile:pathInDocumentDirectory(@"myTeam")];
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+	/*
+	 Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+	 */
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+	[NSKeyedArchiver archiveRootObject:self.myTeam toFile:pathInDocumentDirectory(@"myTeam")];
+}
+
+@end
