5662267f302c374455c2803ecf97b001e488c2bf
Niklas Ingholt <niklas@ingholt.com>
2011-08-10 21:35:42 +0200

    Skeleton for editing match results.

---
+++ PartilleTennis/Controllers/EditMatchResultController.m
@@ -0,0 +1,76 @@
+//
+//  EditMatchResultController.m
+//  PartilleTennis
+//
+//  Created by Niklas Ingholt on 2011-08-10.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "EditMatchResultController.h"
+
+@implementation EditMatchResultController
+
+- (id)init
+{
+    self = [super initWithNibName:@"EditMatchResultView" bundle:nil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+	return 2;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+	return 5;
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+	return [NSString stringWithFormat:@"%d", row];
+}
+
+
+#pragma mark - View lifecycle
+
+/*
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView
+{
+}
+*/
+
+/*
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+*/
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
