6ff149e414fc30f1ef1afade059c55d2a2288519
Niklas Ingholt <niklas@ingholt.com>
2011-08-10 21:26:52 +0200

    Skeleton for editing match results.

---
+++ PartilleTennis/Controllers/EditMatchResultController.h
@@ -0,0 +1,13 @@
+//
+//  EditMatchResultController.h
+//  PartilleTennis
+//
+//  Created by Niklas Ingholt on 2011-08-10.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditMatchResultController : UIViewController
+
+@end
