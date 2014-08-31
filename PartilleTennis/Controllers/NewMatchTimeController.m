7cb4640c853073a6dd20cc4c753971e0d5aa7bb7
niklasi <niklas@ingholt.com>
2014-09-01 00:08:01 +0200

    Clean up.

--- PartilleTennis/Controllers/NewMatchTimeController.m
+++ PartilleTennis/Controllers/NewMatchTimeController.m
@@ -22,14 +22,6 @@
     return self;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    // Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc that aren't in use.
-}
-
 
 #pragma mark - View lifecycle
 
@@ -40,20 +32,7 @@
 		self.timePicker.date = self.match.postponedToDate;
 	}
 }
-/*
-// Implement loadView to create a view hierarchy programmatically, without using a nib.
-- (void)loadView
-{
-}
-*/
 
-/*
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-}
-*/
 
 - (void)viewWillDisappear:(BOOL)animated
 {
@@ -71,13 +50,10 @@
 	[self setTimePicker:nil];
 	[self setTimePickerSegment:nil];
 	[super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    // Return YES for supported orientations
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
